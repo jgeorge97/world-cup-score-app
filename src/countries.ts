911c434fbc543b456cd3626678ae30df3fe6cc8e
Kishor V <developer.kishor@gmail.com>
2022-11-28 18:44:44 +0530

    Handle non-existing countries

--- src/countries.ts
+++ src/countries.ts
@@ -1,10 +1,14 @@
-import countrycodes from "./country-codes.json" assert {type: 'json'}
+import countrycodes from "@/country-codes.json" assert {type: 'json'}
 
 const countrycodemap = new Map<string, any>(Object.entries(countrycodes));
 const flagurl = "https://flagcdn.com/w160/%s.png";
 
 export default {
   getflag: (code: string) => {
-    return flagurl.replace('%s', countrycodemap.get(code).a2.toLowerCase());
+    if (countrycodemap.get(code) !== undefined) {
+      return flagurl.replace('%s', countrycodemap.get(code).a2.toLowerCase());
+    }
+    console.error(`Country not found: ${code}`);
+    return "";
   }
 }
